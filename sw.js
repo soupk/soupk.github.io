040c0e6cf7b857a7855ddf62aeb39bffb1831cbc
soupk <soupk@foxmail.com>
2023-06-14 20:01:39 +0800

    20230614

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230614.170019';
+const cacheName = 'chirpy-20230614.195840';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
