96399b0e331f44dd526e7cdb6f73ff1c967d342d
soupk <soupk@git.com>
2024-07-09 01:56:58 +0800

    20240709

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240621.152003';
+const cacheName = 'chirpy-20240709.014912';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
