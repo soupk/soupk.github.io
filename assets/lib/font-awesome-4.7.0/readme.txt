550fc24cff5c95b0963ff4ae62e0686cd512d17c
soupk <soupk@git.com>
2025-05-07 19:12:34 +0800

    20250507

---
+++ assets/lib/font-awesome-4.7.0/readme.txt
@@ -0,0 +1,10 @@
+欢迎下载  Font Awesome 4.7.0 版!
+
+
+欢迎访问 Font Awesome 中文网,网址 http://fontawesome.p2hp.com
+
+想使用Font Awesome 4.7.0 字体为基础，免费制作超过670+ 个超个性化图标！生成png透明图标提供免费下载??
+
+欢迎访问:Icon图标在线制作工具
+http://icon.p2hp.com/
+
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250430.172323';
+const cacheName = 'chirpy-20250507.190819';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
